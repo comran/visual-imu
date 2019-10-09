8831ca146c4abe3e505c08ef68063f8e8d334938
Comran Morshed <comranmorsh@gmail.com>
2019-10-09 12:25:33 -0700

    Add different methods of performing optical flow.

---
+++ src/sandbox/dense_optical_flow.py
@@ -0,0 +1,39 @@
+import numpy as np
+import cv2 as cv
+import argparse
+
+def resize_res(frame):
+    return cv.resize(frame,
+            (int(frame.shape[1] * 0.25), int(frame.shape[0] * 0.25)),
+            interpolation = cv.INTER_AREA)
+
+parser = argparse.ArgumentParser(description='This sample demonstrates Lucas-Kanade Optical Flow calculation. \
+                                              The example file can be downloaded from: \
+                                              https://www.bogotobogo.com/python/OpenCV_Python/images/mean_shift_tracking/slow_traffic_small.mp4')
+parser.add_argument('image', type=str, help='path to image file')
+args = parser.parse_args()
+cap = cv.VideoCapture(cv.samples.findFile(args.image))
+ret, frame1 = cap.read()
+frame1 = resize_res(frame1)
+prvs = cv.cvtColor(frame1,cv.COLOR_BGR2GRAY)
+hsv = np.zeros_like(frame1)
+hsv[...,1] = 255
+while(1):
+    ret, frame2 = cap.read()
+    frame2 = resize_res(frame2)
+    next = cv.cvtColor(frame2,cv.COLOR_BGR2GRAY)
+    flow = cv.calcOpticalFlowFarneback(prvs,next, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+    mag, ang = cv.cartToPolar(flow[...,0], flow[...,1])
+    hsv[...,0] = ang*180/np.pi/2
+    hsv[...,2] = cv.normalize(mag,None,0,255,cv.NORM_MINMAX)
+    bgr = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
+    cv.imshow('frame2',bgr)
+    cv.imshow('frame2_raw',frame2)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+    elif k == ord('s'):
+        cv.imwrite('opticalfb.png',frame2)
+        cv.imwrite('opticalhsv.png',bgr)
+    prvs = next
+
